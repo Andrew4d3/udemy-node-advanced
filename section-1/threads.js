4a46fc8844782d80fb4c4a7ca6a43d695cc6cd65
Andrew4d3 <andrew4d3@gmail.com>
2019-05-16 23:34:42 -0400

    Section 1 - Lecture 10

--- section-1/threads.js
+++ section-1/threads.js
@@ -1,5 +1,14 @@
 const crypto = require("crypto");
 
+const start = Date.now();
+// If we only run this call, it will take like 771 - 1000ms aprox (depending on how fast your CPU is)
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-  // Callback code goes here
+  console.log("1:", Date.now() - start);
 });
+
+// But if we run this call too, it will take a similar value to what we saw before
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("2:", Date.now() - start);
+});
+
+// How is this possible if NodeJS is supposed to be single threading?
