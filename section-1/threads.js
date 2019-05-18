194e48b278ef8437850ec7d5d761cdc30b34ed89
Andrew4d3 <andrew4d3@gmail.com>
2019-05-18 14:28:59 -0400

    Section 2 - Lecture 12

--- section-1/threads.js
+++ section-1/threads.js
@@ -1,7 +1,8 @@
 const crypto = require("crypto");
 
 const start = Date.now();
-// If we only run this call, it will take like 771 - 1000ms aprox (depending on how fast your CPU is)
+
+// Now we are going to run 5 pbkdf2 calls
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("1:", Date.now() - start);
 });
@@ -11,4 +12,24 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("2:", Date.now() - start);
 });
 
-// How is this possible if NodeJS is supposed to be single threading?
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("3:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("4:", Date.now() - start);
+});
+
+// The last one will take around the double of the first four. Because we are exceeding the thread pool size
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("5:", Date.now() - start);
+});
+
+/*
+$ node section-1/threads.js 
+4: 805
+1: 808
+3: 818
+2: 824
+5: 1572
+*/
