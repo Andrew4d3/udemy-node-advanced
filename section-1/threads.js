94448db0dd88332663439ebd80bd0357a25f0e62
Andrew4d3 <andrew4d3@gmail.com>
2019-05-18 20:24:38 -0400

    Section 1 - Lecture 13

--- section-1/threads.js
+++ section-1/threads.js
@@ -1,17 +1,20 @@
+// Let's change the threadpool size
+process.env.UV_THREADPOOL_SIZE = 2;
+
 const crypto = require("crypto");
 
 const start = Date.now();
 
-// Now we are going to run 5 pbkdf2 calls
+// Now you will see how these two calls finish first
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("1:", Date.now() - start);
 });
 
-// But if we run this call too, it will take a similar value to what we saw before
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("2:", Date.now() - start);
 });
 
+// And then these two...
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("3:", Date.now() - start);
 });
@@ -20,16 +23,18 @@ crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("4:", Date.now() - start);
 });
 
-// The last one will take around the double of the first four. Because we are exceeding the thread pool size
+// And last, this one...
 crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
   console.log("5:", Date.now() - start);
 });
 
+// In summary: Only two calls will run in paralell. Because we have the threadpool set up in 2
+
 /*
 $ node section-1/threads.js 
-4: 805
-1: 808
-3: 818
-2: 824
-5: 1572
+2: 777
+1: 780
+3: 1555
+4: 1558
+5: 2303
 */
